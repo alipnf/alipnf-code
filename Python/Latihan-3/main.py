bccb69b720e0cacc9e36fad296ffe66897a39b73
alipnf <alifnur1224@gmail.com>
2022-10-12 21:04:59 +0700

    add python latihan-3

--- Python/Latihan-3/main.py
+++ Python/Latihan-3/main.py
@@ -1,8 +1,10 @@
-from tkinter import *
+import time
+start_time = time.time()
+print('hello world')
+a = 7
+L = 3.14 * a * a
+print(L)
+print(time.time() - start_time, "detik")
+#ini adalah luas lingkaran
+#cara megcompile, buka terminal dan tuliskan "python -m py_compile main.py"
 
-app = Tk()
-
-app.geometry('400x600')
-app.configure(bg='#38c2bb')
-
-app.mainloop()

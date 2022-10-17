4665843fd4dc4e1f5d3b99a744f61a842074fc2b
alipnf <alifnur1224@gmail.com>
2022-10-17 12:35:00 +0700

    add latihan python 4

--- Python/Latihan-4/main.py
+++ Python/Latihan-4/main.py
@@ -1,1 +1,19 @@
-print("hello")
+#tipe data : Angka satuan (integer)
+data_integer =  1
+print("data :", data_integer)
+print("bertipe :", type(data_integer))
+
+#tipe data : Angka satuan dengan koma (float)
+data_float =  1.5
+print("data :", data_float)
+print("bertipe :", type(data_float))
+
+#tipe data : kata dengan satuan hurud (string)
+data_string =  "jamal"
+print("data :", data_string)
+print("bertipe :", type(data_string))
+
+#tipe data : biner true/false  (boolean)
+data_bool = False  
+print("data :", data_bool)
+print("bertipe :", type(data_bool))

abbcb65db2c08f5dd14272d46096100cb3de3301
alipnf <alifnur1224@gmail.com>
2022-11-24 10:18:53 +0700

    add bilnagan.py & remoce operasi.py

---
+++ Python/python-perulangan/bilangan.py
@@ -0,0 +1,10 @@
+x = int(input('masukan nilai awal :'))
+y = int(input('masukan nilai akhir :'))
+
+while (x <= y):
+    print ("")
+    i = 1
+    while (i <= x):
+        print (x , "", end = "")
+        i = i+1
+    x = x+1
--- Python/python-perulangan/operasi.py
+++
@@ -1,6 +0,0 @@
-x = int(input('masukan nilai awal :'))
-y =  int(input('masukan nilai akhir'))
-while (x <= y):
-    if (x%5==0):
-        print (x)
-    x = x+1

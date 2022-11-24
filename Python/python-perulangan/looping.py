05722ab9ec20b747c5b6967c89cdfdb9b378ada1
alipnf <alifnur1224@gmail.com>
2022-11-24 12:17:26 +0700

    edit penggunaan for

---
+++ Python/python-perulangan/looping.py
@@ -0,0 +1,23 @@
+#contoh penggunaan while loop
+ulang = 1
+while (ulang <=10 ):
+    print ('Perulangan ke :', ulang)
+    ulang = ulang + 1
+print ('perulangan selesai')
+
+#contoh penggunaan for sederhana 
+ulang = [1,3,5,7]
+for x in ulang:
+    print (x)
+
+#contoh penggunaan for lain
+matkul1 = ["TTKI","Agama","Pancasila"]
+matkul2 = ["Diskrit","pemrograman dasar", "Sistem digital"]
+print ("rifki mengambil matkul :")
+for x in matkul1:
+    print (x)
+print ("")
+print ("alif mengambil matkul :")
+for y in matkul2:
+    print (y)
+    
--- Python/python-perulangan/while.py
+++
@@ -1,6 +0,0 @@
-#contoh penggunaan while loop
-ulang = 1
-while (ulang <=10 ):
-    print ('Perulangan ke :', ulang)
-    ulang = ulang + 1
-print ('perulangan selesai')

5955f418aba6401326aab8b591fb092485a004d3
alipnf <alifnur1224@gmail.com>
2022-11-24 18:34:11 +0700

    edit looping.py

---
+++ Python/python-perulangan/angka.py
@@ -0,0 +1,4 @@
+x = int(input('masukan angka'))
+while (x<10):
+    print(x)
+    x = x+1
--- Python/python-perulangan/looping.py
+++ Python/python-perulangan/looping.py
@@ -1,6 +1,11 @@
 #contoh penggunaan while loop
-ulang = 1
-while (ulang <=10 ):
-    print ('Perulangan ke :', ulang)
-    ulang = ulang + 1
-print ('perulangan selesai')
+#ulang = 1
+#while (ulang <=10 ):
+#    print ('Perulangan ke :', ulang)
+#    ulang = ulang + 1
+#print ('perulangan selesai')
+
+#contoh for sederhana
+makan = ['tempe', 'pecel', 'rames', 'telus']
+for x in makan:
+    print ('rifiki suka makan', x)

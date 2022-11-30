50f1c26d31244284cce33ac7a5cafcb0d3b7b512
alipnf <alifnur1224@gmail.com>
2022-11-30 20:31:20 +0700

    add tugas.py

---
+++ Python/python-perulangan/tugas.py
@@ -0,0 +1,21 @@
+x = 1
+y = 5
+z = x
+
+# sudah benar
+while (x <= y):
+    v = 1
+    print ("")
+    while (v <= x):
+        print (x, "", end = "")
+        v = v + 1
+    x = x + 1
+x = x - 2 
+
+while (x >= z):
+    v = 1
+    print ("")
+    while (v <= x):
+        print (x, "", end = "")
+        v = v + 1
+    x = x - 1

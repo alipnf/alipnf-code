9caaeb655aa2e3662a35ec8b5a668d4831b2ca87
alipnf <alifnur1224@gmail.com>
2022-12-01 07:55:07 +0700

    edit tugas.py

--- Python/python-perulangan/tugas.py
+++ Python/python-perulangan/tugas.py
@@ -1,14 +1,13 @@
 x = 1
-y = 5
+y = 4
 
-while (x < y):
+while (x <= y):
     v = 1
     print ("")
     while (v <= x):
         print (x, "", end = "")
         v = v + 1
     x = x + 1
-
 while (1 <= x):
     v = 1
     print ("")
